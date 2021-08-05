8e8ebcd8524acb25029c6c8a991bd770753dd5cb
liuyue <>
2021-08-05 18:22:04 +0800

    Revert "Revert "Revert "不推送提交不了后面"""
    This reverts commit 2a94fcd36dd5299971a4a7e2938e6a22ec6abcdf.

--- class_0730_apiauto/__init__.py
+++
@@ -1,4 +0,0 @@
-# @Time:2021/7/30 3:12 下午
-# @Author:liuyue
-# @Email:liuyue@soyoung.com
-# @coding:utf-8
--- class_other/__init__.py
+++
@@ -1,4 +0,0 @@
-# @Time:2021/8/4 7:28 下午
-# @Author:liuyue
-# @Email:liuyue@soyoung.com
-# @coding:utf-8
